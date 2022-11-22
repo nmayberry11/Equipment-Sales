dae69b921f033311ba1c8ac6929aa94398715cae
Nick <missourimayberry@gmail.com>
2022-11-21 18:25:40 -0600

    Project Reboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatEquipmentComponent } from './cat-equipment/cat-equipment.component';
+import { AlliedEquipmentComponent } from './allied-equipment/allied-equipment.component';
+import { TrailersComponent } from './trailers/trailers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatEquipmentComponent,
+    AlliedEquipmentComponent,
+    TrailersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cat-equipment/cat-equipment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-equipment',
+  templateUrl: './cat-equipment.component.html',
+  styleUrls: ['./cat-equipment.component.css']
+})
+export class CatEquipmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
